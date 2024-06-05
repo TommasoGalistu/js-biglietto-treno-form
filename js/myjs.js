c373d101561e109ac96f4187d2e7e3dc3590e77b
TommasoGalistu <tommasogalistu92@gmail.com>
2024-06-05 15:14:22 +0200

    inserimento numero carrozza

--- js/myjs.js
+++ js/myjs.js
@@ -25,12 +25,18 @@ contButton.addEventListener('click', (event) => {
     }
 });
 
+// variabili per assegnazione posto treno
+let posto = 1;
+let carr = 1;
+
+// funzione che si attiva al click di genera
 function genera(){
     let risultato;
-    
+
+    // calcolo costi in base all'età selezionata
     if(eta.value === 'minorenne'){
         risultato = (kmPercorsi.value * 0.21) * 0.8;
-        tipoTariffa.textContent = 'Tariffa minorenni';
+        tipoTariffa.textContent = 'Tariffa Minori';
     }else if(eta.value === 'anziano'){
         risultato = (kmPercorsi.value * 0.21) * 0.6;
         tipoTariffa.textContent = 'Over 65';
@@ -38,7 +44,21 @@ function genera(){
         risultato = (kmPercorsi.value * 0.21);
         tipoTariffa.textContent = 'Tariffa Standard';
     }
+    // inserimento costo personalizzato per età
     costoBigl.textContent = risultato.toFixed(2) + '€';
+
+    // inserimento nome e cognome in maiuscolo
     textNomePasseggero.textContent = nomeCognome.value.toUpperCase();
+
+    // inserimento numero carrozza
+    carrozzaNr.textContent = `Carr ${carr} - Posto ${posto}`
+    posto++
+    if(posto === 50){
+        carr++
+        posto = 1
+    }else if(carr === 9){
+        let finePosti = 'In piedi'
+        carrozzaNr.textContent = finePosti
+    }
     
 };
